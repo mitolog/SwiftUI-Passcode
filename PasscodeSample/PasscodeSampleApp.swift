a02617081f2d801fcb2d7b74aa6e5380b71cc04b
mito_log <hkyhm5l@gmail.com>
2022-04-04 18:51:25 +0900

    Initial Commit

---
+++ PasscodeSample/PasscodeSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasscodeSampleApp.swift
+//  PasscodeSample
+//
+//  Created by Yuhei Miyazato on 2022/04/04.
+//
+
+import SwiftUI
+
+@main
+struct PasscodeSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
